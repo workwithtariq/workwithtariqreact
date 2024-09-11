cf75fd48ca9758a5d3613934149bde79a87429c2
Tariq <workwithtariq@gmail.com>
2024-09-11 22:50:48 +0600

    add icon hover scale

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -16,7 +16,7 @@ const Button = ({
   const variants = {
     primary: `${bigButtonClasses} bg-black text-white hover:bg-gray-800`,
     secondary: `${bigButtonClasses} border-2 border-black text-black hover:bg-black hover:text-white`,
-    icon: `text-gray-600 hover:text-black w-10`, // New icon variant for social buttons
+    icon: `text-gray-600 hover:text-black w-10 hover:bg-blue-200 hover:scale-110`, // New icon variant for social buttons
   };
 
   if (disabled)
