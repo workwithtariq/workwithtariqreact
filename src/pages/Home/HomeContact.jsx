3849d72764f8813cc7c492bfca1d9292190b02de
Tariq <workwithtariq@gmail.com>
2024-09-11 22:27:00 +0600

    make clickable email and phone

--- src/pages/Home/HomeContact.jsx
+++ src/pages/Home/HomeContact.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import SocialIcons from "../../components/SocialIcons/SocialIcons";
 import HomeSectionHeader from "./HomeSectionHeader";
-import FacebookIcon from "../../assets/fb.svg";
-import LinkedInIcon from "../../assets/in.svg";
-import WhatsAppIcon from "../../assets/wa.svg";
-import YoutubeIcon from "../../assets/yt.svg";
-import Github from "../../assets/github.svg";
 import MailIcon from "../../assets/mail.svg";
 import PhoneIcon from "../../assets/phone.svg";
 
@@ -16,11 +11,11 @@ const HomeContact = () => {
       <div className="border border-slate-400 w-fit mx-auto p-6 rounded-[32px] flex flex-col gap-4">
         <div className="flex flex-wrap items-center gap-4 justify-center">
           <SocialIcons disabled={true} iconSrc={MailIcon} />
-          <span className="text-xl">hello@workwithtariq.com</span>
+          <a href="mailto:hello@workwithtariq.com" className="text-xl">hello@workwithtariq.com</a>
         </div>
         <div className="flex flex-wrap items-center gap-4 justify-center">
           <SocialIcons disabled={true} iconSrc={PhoneIcon} />
-          <span className="text-xl">+8801517010100</span>
+          <a href="tel:+8801517010100" className="text-xl">+8801517010100</a>
         </div>
         
       </div>
