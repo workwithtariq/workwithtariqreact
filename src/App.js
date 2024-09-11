3924a1550e757c9f88b324ab8f66e02e5c68d501
Tariq <workwithtariq@gmail.com>
2024-09-11 12:52:05 +0600

    applying suspense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
-
+import React, { Suspense } from "react";
 import Header from "./components/Header/Header";
 import "./styles/App.css";
 import AppRoutes from "./routes/AppRoutes";
+
+// Define a fallback component
+const Loading = () => <div>Loading...</div>;
 
 export const App = () => {
   return (
     <div className="max-w-[1280px] mx-auto px-4 sm:px-20 md:px-30">
       <Header />
-      <AppRoutes />
+      {/* Wrap AppRoutes with Suspense */}
+      <Suspense fallback={<Loading />}>
+        <AppRoutes />
+      </Suspense>
     </div>
   );
 };
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,22 +1,28 @@
-// src/Routes.jsx
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import About from "../pages/About/About";
-import Projects from "../pages/Projects/Projects";
-import Project from "../pages/Projects/Project/Project";
-import Contact from "../pages/Contact/Contact";
- 
+
+// Dynamically import pages
+const Home = lazy(() => import('../pages/Home/Home'));
+const About = lazy(() => import('../pages/About/About'));
+const Projects = lazy(() => import('../pages/Projects/Projects'));
+const Project = lazy(() => import('../pages/Projects/Project/Project'));
+const Contact = lazy(() => import('../pages/Contact/Contact'));
+
+// Define a fallback component
+const Loading = () => <div>Loading...</div>;
+
 const AppRoutes = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:id" element={<Project />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<Project />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
